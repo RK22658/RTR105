3b0ad4745260e88261f08e4b7abc91ce3d75c01f
RK22658 <144732504+RK22658@users.noreply.github.com>
2024-02-14 18:28:43 +0200

    Update task5.c

--- task_5/task5.c
+++ task_5/task5.c
@@ -1,50 +1,75 @@
 #include <stdio.h>
-#include <limits.h>
 
-int main() {
-  int number;
-  char data_type;
-
-  printf("Enter a decimal number: ");
-  scanf("%d", &number);
-
-  printf("Enter data type (char(c) / int(i) / long long(l)): ");
-  scanf(" %c", &data_type);
-
-  if (data_type == 'c') {
-    char factorial = 1;
-    for (int i = 1; i <= number; i++) {
-      if (factorial > 255) {
-        printf("Cannot calculate factorial with char data type.\n");
-        return 0;
-      }
-      factorial = factorial * i;
+void main()
+{
+    long long num;
+    char charNum = 1;
+    long long longNum = 1;
+    int intNum = 1;
+    char choice;
+    printf(" Enter a Decimal Number: ");
+    scanf("%lld99", &num);
+    printf(" Enter the Data type (c) for char, (i) for int, (l) for long long \n: ");
+    scanf(" %c99", &choice);
+    if(choice == 'c')
+    {
+        char reference = 1;
+        for(int counter = 1;counter < num; counter++)
+        {
+        
+            charNum *= (counter + 1);
+            reference *= counter;
+            if((charNum/(counter + 1)) != reference){
+                
+                printf("Selected Data type is too small to store number %lld\n", num);
+            
+                break;
+            }
+        }
+        if(charNum == reference * num)
+        {
+            printf("Factorial: %d\n", num, charNum); 
+        }
+        
+    
+    
+    }else if(choice == 'i')
+    {
+        int reference = 1;
+        for(int counter = 1;counter < num; counter++)
+        {
+        
+            intNum *= (counter + 1);
+            reference *= counter;
+            if(intNum/(counter + 1) != reference){
+                
+                printf("Selected data type is too small to store number %lld\n", num);
+            
+                break;
+            }
+        }
+        if(intNum == reference * num)
+        {
+            printf("Factorial: %d\n", num, intNum); 
+        }
+    }else if(choice == 'l')
+    {
+        long long reference = 1;
+        for(long long counter = 1;counter < num; counter++)
+        {
+        
+            longNum *= (counter + 1);
+            reference *= counter;
+            if(longNum/(counter + 1) != reference){
+                
+                printf("Selected data type is too small to store number %lld\n", num);
+            
+                break;
+            }
+        }
+        if(longNum == reference * num)
+        {
+            printf("Factorial: %lld\n", num, longNum); 
+        }
     }
-    printf("The factorial of %d is %d (char data type).\n", number, factorial);
-  } else if (data_type == 'i') {
-    int factorial = 1;
-    for (int i = 1; i <= number; i++) {
-      if (factorial > INT_MAX) {
-        printf("Cannot calculate factorial with int data type.\n");
-        return 0;
-      }
-      factorial = factorial * i;
-    }
-    printf("The factorial of %d is %d (int data type).\n", number, factorial);
-  } else if (data_type == 'l') {
-    long long factorial = 1;
-    for (long long i = 1; i <= number; i++) {
-      if (factorial > LLONG_MAX) {
-        printf("Cannot calculate factorial with long long data type.\n");
-        return 0;
-      }
-      factorial = factorial * i;
-    }
-    printf("The factorial of %d is %lld (long long data type).\n", number, factorial);
-  } else {
-    printf("Invalid data type.\n");
-    return 0;
-  }
-
-  return 0;
 }
